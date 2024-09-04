2565d57a4c54ccc2dcb3aa350ed2dedb9b0f4253
BhushanChowta <bhushanchowta@gmail.com>
2024-09-04 14:01:02 +0530

    server.js restruxctre

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,51 @@
+// Load environment variables
+require('dotenv').config();
+
+// Import dependencies
 const express = require('express');
-const axios = require('axios');
 const bodyParser = require('body-parser');
 const { google } = require('googleapis');
-const postRouter = require('./routes/post')
-const blogRouter = require('./routes/blog')
-const userActionRouter = require('./routes/userAction')
-
-require('dotenv').config();
 const cors = require('cors');
+const mongoose = require('mongoose');
 
+// Import routers
+const postRouter = require('./routes/post');
+const blogRouter = require('./routes/blog');
+const userActionRouter = require('./routes/userAction');
+
+// Initialize the app
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware setup
 app.use(cors()); // Enable CORS
-app.use(bodyParser.json());
-app.use('/api',postRouter.routes);
-app.use('/api',blogRouter.routes);
-app.use('/api',userActionRouter.routes);
+app.use(bodyParser.json()); // Parse incoming JSON requests
 
-const blogger = google.blogger('v3');
+// Route setup
+app.use('/api', blogRouter.routes);
+app.use('/api', postRouter.routes);
+app.use('/api', userActionRouter.routes);
 
+// Setup OAuth2 client
 const oauth2Client = new google.auth.OAuth2(
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
     process.env.REDIRECT_URI
 );
-
 oauth2Client.setCredentials({
     refresh_token: process.env.REFRESH_TOKEN,
 });
 
-
-// MongoDB Connect
-const mongoose = require('mongoose');
+// MongoDB connection
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => console.log('Connected to MongoDB Atlas'))
-.catch((err) => console.error('Could not connect to MongoDB Atlas...', err));
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch((err) => console.error('Could not connect to MongoDB Atlas...', err));
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
